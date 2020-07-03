1ff9e303e55e4c0eb543f1d04ff39af3fd315ee8
Yury Buslovsky <yury.buslovsky@gmail.com>
2020-07-03 16:02:44 +0500

    Initial Commit

---
+++ MulticastDelegate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MulticastDelegate
+//
+//  Created by Yury Buslovsky on 03.07.2020.
+//  Copyright © 2020 Napoleon IT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
